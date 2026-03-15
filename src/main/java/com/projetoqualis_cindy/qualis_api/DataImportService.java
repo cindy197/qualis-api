48dd07121d78d03ed6c64e29ec885634ff103b24
Cindy Pessoa <cindypessoa17@gmail.com>
2026-03-14 23:41:07 -0300

    Update DataImportService

--- src/main/java/com/projetoqualis_cindy/qualis_api/DataImportService.java
+++ src/main/java/com/projetoqualis_cindy/qualis_api/DataImportService.java
@@ -1,9 +1,8 @@
-package br.com.cindy.qualis;
+package com.projetoqualis_cindy.qualis_api;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.projetoqualis_cindy.qualis_api.PeriodicoRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +11,30 @@ import java.io.InputStream;
 import java.util.List;
 
 @Service
-public class DataImportService {}
+public class DataImportService {
 
-@Autowired
-private PeriodicoRepository repository;
+    @Autowired
+    private PeriodicoRepository repository;
 
-@PostConstruct
-    public void importData(){
-    
+    @PostConstruct
+    public void importData() {
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("qualis.csv");
+
+            CsvMapper mapper = new CsvMapper();
+            CsvSchema schema = CsvSchema.emptySchema().withHeader();
+
+            MappingIterator<Periodico> it = mapper.readerFor(Periodico.class)
+                    .with(schema)
+                    .readValues(is);
+            List<Periodico> todos = it.readAll();
+
+            repository.saveAll(todos);
+
+            System.out.println(">>> SUCESSO: " + repository.count() + " registros carregados!");
+
+        } catch (Exception e) {
+            System.err.println("Erro ao importar dados: " + e.getMessage());
+        }
+    }
 }
