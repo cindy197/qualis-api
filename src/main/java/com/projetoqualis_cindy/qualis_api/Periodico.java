3b528e9ddb4bc10c8ebf10984118f60ec0eefa60
Cindy Pessoa <cindypessoa17@gmail.com>
2026-03-15 18:43:04 -0300

    Update Periodico
    Ajuste de Erro Interno ao conectar a API com o front

--- src/main/java/com/projetoqualis_cindy/qualis_api/Periodico.java
+++ src/main/java/com/projetoqualis_cindy/qualis_api/Periodico.java
@@ -1,26 +1,30 @@
 package com.projetoqualis_cindy.qualis_api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-@JsonPropertyOrder({"ISSN", "Título", "Área de Avaliação", "Estrato"})
-
 public class Periodico {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define que o banco gera o 1, 2, 3... automaticamente
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JsonProperty("ISSN")
+    @Column(name = "issn")
     private String issn;
+
     @JsonProperty("Título")
+    @Column(name = "titulo")
     private String titulo;
+
     @JsonProperty("Área de Avaliação")
+    @Column(name = "area_avaliacao")
     private String areaAvaliacao;
+
     @JsonProperty("Estrato")
+    @Column(name = "estrato")
     private String estrato;
 }
